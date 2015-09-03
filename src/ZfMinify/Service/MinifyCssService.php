286bd3ac6a8a83c22ea2e49a35c0d6da8f495e2e
Vahag Dudukgian <vahag@vcomedia.com>
2015-09-02 22:44:31 -0700

    fixed variable spelling error

--- src/ZfMinify/Service/MinifyCssService.php
+++ src/ZfMinify/Service/MinifyCssService.php
@@ -5,6 +5,6 @@ use Minify_CSS;
 
 class MinifyCssService implements MinifyServiceInterface {
   public function minify($content, $options = array()) {
-    return Minify_CSS::minify($content, $optionss);
+    return Minify_CSS::minify($content, $options);
   }
 }
